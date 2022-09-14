9be0e6428907eea2abb0518ee250a88350d33701
captvin <alpinoke82@gmail.com>
2022-09-14 16:38:09 +0700

    reapir login controller

--- server3/controllers/login.controller.js
+++ server3/controllers/login.controller.js
@@ -1,38 +1,48 @@
-const { User } = require('@models')
+const { user } = require('@models')
 const { sign } = require('jsonwebtoken')
 require('dotenv').config()
 
 
-async function login(req, res, next) {
+    async function login(req, res, next) {
 
-    
-        const { username } = req.body
+        try{
 
-        const result = await User.findOne({username})
-        if(!result){
-            return res.status(404).send({message:"User not found"});
+            const { username } = req.body
+
+            const result = await user.findOne({where: {username}})
+            if(!result){
+                return res.status(404).send({message:"User not found"});
+            }
+            
+            const { password } = req.body
+            const password_match = compareSync(password, result.password)
+
+            if(password_match){
+                result.password = undefined
+                const jsonwebtoken = sign(
+                    {results},
+                    process.env.JWT_SECRET_KEY,
+
+                )
+                return response_format(res,1,"Login Successful",{account: results, token: jsonwebtoken})
+            }
+
+            else{
+                return response_format(res, 0, "Wrong password!",).status(403)
+            }
+        } catch (err) {
+            console.log(err)
         }
         
-        const { password } = req.body
-        const password_match =  compareSync(password, result.password)
-
-        if(password_match){
-            result.password = undefined
-            const jsonwebtoken = sign(
-                {results},
-                process.env.JWT_SECRET_KEY,
-
-            )
-            return response_format(res,1,"Login Successful",{account: results, token: jsonwebtoken})
-        }
-
-        else{
-            return response_format(res, 0, "Wrong password!",).status(403)
-        }
     
-}
+    }
 
-module.exports = {
-    login
-}
+    module.exports = {
+        login
+    }
+
+
+
+
+
    
