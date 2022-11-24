39c5bb2600e63cc0d9f6f112a55364de5adc5e71
captvin <alpinoke82@gmail.com>
2022-11-24 19:15:58 +0700

    Add detail transaksi API

--- server3/controllers/detail.controller.js
+++ server3/controllers/detail.controller.js
@@ -1,90 +1,81 @@
-const { detail, transaksi, paket, user, member } = require('@models')
+const { detail} = require('@models')
 const { NotFound, Forbidden } = require('http-errors')
+const detailSchema = require('../validations/detail.schema')
 
 async function findAll(req, res, next) {
-    if (req.user.abilities.cannot('read', transaksi)) {
+    if (req.user.abilities.cannot('read', detail)) {
         return next(Forbidden())
     }
     const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || 15
+    const limit = Number(req.query.limit) || 10
 
     const options = {
         offset: (page - 1) * limit,
         limit,
-        order: [['createdAt', 'ASC']],
-        include: [],
+        order: [
+            ['createdAt', 'ASC']
+        ],
         where: {}
     }
 
-    const { tahun, bulan } = req.query
+    const { id_paket } = req.query
 
-    tahun
-        ? options.where.tahun = tahun
-        : bulan ? options.where.bulan = bulan
-            : null
+    if (id_paket) {
+        options.where['id_paket'] = id_paket
+    }
 
-    const result = await transaksi.findAndCountAll(options)
+    const result = await detail.findAndCountAll(options)
     const totalPage = Math.ceil(result.count / limit)
 
     res.json({ currentPage: page, totalPage, rowLimit: limit, ...result })
 }
 
 async function findById(req, res, next) {
-    if (req.user.abilities.cannot('read', transaksi)) {
+    if (req.user.abilities.cannot('read', detail)) {
         return next(Forbidden())
     }
-    const result = await transaksi.findByPk(req.params.id)
+    const relations = []
+    if (req.query.getDetail === 'true') {
+        relations.push('transaksi')
+    }
+    const detail = await detail.findByPk(req.params.id, { include: relations })
     result
-        ? res.json(result)
+        ? res.send(detail)
         : next(NotFound())
 }
 
 async function create(req, res, next) {
-    if (req.user.abilities.cannot('create', transaksi)) {
+    if (req.user.abilities.cannot('create', detail)) {
         return next(Forbidden())
     }
+    // const id_paket = req.body.id_paket
+    // const total = await paket.findByPk(req.body.id_paket)
     const { body } = req
-    const result = await transaksi.create(body)
+    const result = await detail.create(body)
     res.json(result)
 }
 
 async function update(req, res, next) {
-    let trans = await transaksi.findByPk(req.params.id)
-    if (!trans) {
-        return next(NotFound())
-    } else if (req.user.abilities.cannot('update', transaksi)) {
+    if (req.user.abilities.cannot('update', detail)) {
         return next(Forbidden())
     }
-
-    const result = await transaksi.update(req.body)
-    return res.send({
-        message: "Successfully updated transaksi",
-        fields: req.body,
-        result
-    })
+    const { id } = req.params
+    const { body } = req
+    const result = await detail.update(body, { where: { id } })
+    result[0]
+        ? res.json({ message: 'Successfully updated' })
+        : next(NotFound())
 }
 
 async function remove(req, res, next) {
-    let trans = await transaksi.findByPk(req.params.id)
-    if (!trans) {
-        return next(NotFound())
-    } else if (req.user.abilities.cannot('delete', transaksi)) {
+    if (req.user.abilities.cannot('delete', detail)) {
         return next(Forbidden())
     }
-
-    try {
-        const result = await transaksi.destroy()
-        return res.send({
-            message: "Successfully deleted transaksi",
-            data: result
-        })
-    } catch (error) {
-        if (error.original.errno == 1451) {
-            return next(Forbidden(
-                "Delete action for Spp causing related Pembayaran records gone!"
-            ))
-        }
-    }
+    const { id } = req.params
+    const result = await detail.destroy({ where: { id } })
+    result === 1
+        ? res.json({ message: 'Successfully deleted' })
+        : next(NotFound())
 }
 
 module.exports = {
